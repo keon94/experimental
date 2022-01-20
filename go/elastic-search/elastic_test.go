cae1577aac2495132358b21254f391889ec805d8
Keon Amini <keon.amini@skael.com>
2022-01-20 13:19:33 -0600

    minor cleanup

--- go/elastic-search/elastic_test.go
+++ go/elastic-search/elastic_test.go
@@ -5,12 +5,13 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"github.com/elastic/go-elasticsearch/v7/esutil"
-	"github.com/tidwall/gjson"
 	"strconv"
 	"sync"
 	"testing"
 	"time"
+
+	"github.com/elastic/go-elasticsearch/v7/esutil"
+	"github.com/tidwall/gjson"
 
 	"github.com/stretchr/testify/require"
 
@@ -20,6 +21,99 @@ import (
 
 	es "github.com/elastic/go-elasticsearch/v7"
 )
+
+func TestIndexAndSearchJSON_QueryByMatch(t *testing.T) {
+	cli := getClient(t)
+	{
+		info, err := cli.Info(func(request *esapi.InfoRequest) {
+			request.Pretty = true
+		})
+		require.NoError(t, err)
+		fmt.Println(info.String())
+		require.NoError(t, info.Body.Close())
+	}
+
+	index1 := "test1"
+	var docID1 string
+	{
+		indexResponses := createIndices(t, func() (*esapi.Response, error) {
+			docID1 = strconv.Itoa(time.Now().Nanosecond())
+			return cli.Index(
+				index1,
+				bytes.NewBuffer(ReadFile(t, "data/file1.json")),
+				cli.Index.WithDocumentID(docID1),
+				cli.Index.WithRefresh("true"),
+			)
+		})
+		fmt.Printf("Index creation responses:\n%s", StructToJSON(t, indexResponses))
+	}
+	{
+		query := fmt.Sprintf(`
+			{
+				"query": {
+					"match": {
+						"title": "%s"
+					}
+				 }
+			}
+		`, "Title1")
+		res, err := cli.Search(
+			cli.Search.WithContext(context.Background()),
+			cli.Search.WithIndex(index1),
+			cli.Search.WithBody(bytes.NewBuffer([]byte(query))),
+			cli.Search.WithTrackTotalHits(true),
+			cli.Search.WithPretty(),
+		)
+		require.NoError(t, err)
+		require.False(t, res.IsError())
+		js := BodyToJSON(t, res.Body)
+		fmt.Printf("SEARCH response:\n%s\n", js)
+		value := gjson.Get(js, "hits.total.value").Float()
+		took := gjson.Get(js, "took").Float()
+		fmt.Printf("[%s] %f hits; took: %fms", res.Status(), value, took)
+		hits := gjson.Get(js, "hits.hits").Array()
+		require.Greater(t, len(hits), 0)
+		for _, hit := range hits {
+			id := hit.Get("_id").Value()
+			src := hit.Get("_source").Value()
+			fmt.Printf(" * ID=%v, %v\n", id, src)
+		}
+	}
+	{
+		res, err := cli.Get(index1, docID1)
+		require.NoError(t, err)
+		require.False(t, res.IsError())
+		js := BodyToJSON(t, res.Body)
+		fmt.Printf("GET response:\n%s\n", js)
+		require.True(t, gjson.Get(js, "found").Bool())
+	}
+}
+
+func getClient(t *testing.T) *es.Client {
+	t.Helper()
+	env := docker.StartEnvironment(
+		&docker.EnvironmentConfig{
+			UpTimeout:        60 * time.Second,
+			DownTimeout:      60 * time.Second,
+			ComposeFilePaths: []string{"docker-compose.yml"},
+		},
+		&docker.ServiceEntry{
+			Name:                "es01",
+			Handler:             GetElasticSearchClient,
+			DisableShutdownLogs: false,
+		},
+		&docker.ServiceEntry{
+			Name:                "es02",
+			DisableShutdownLogs: true,
+		},
+		&docker.ServiceEntry{
+			Name:                "kibana",
+			DisableShutdownLogs: true,
+		},
+	)
+	t.Cleanup(env.Shutdown)
+	return env.Services["es01"].(*es.Client)
+}
 
 func createIndices(t *testing.T, reqs ...func() (*esapi.Response, error)) map[string]*esutil.BulkIndexerResponseItem {
 	responses := make(map[string]*esutil.BulkIndexerResponseItem)
@@ -44,97 +138,4 @@ func createIndices(t *testing.T, reqs ...func() (*esapi.Response, error)) map[st
 	}
 	wg.Wait()
 	return responses
-}
-
-func TestIndexAndSearchJSON_QueryByMatch(t *testing.T) {
-	cli := getClient(t)
-	{
-		info, err := cli.Info(func(request *esapi.InfoRequest) {
-			request.Pretty = true
-		})
-		require.NoError(t, err)
-		fmt.Println(info.String())
-		require.NoError(t, info.Body.Close())
-	}
-
-	titleIndex1 := "test1"
-	var id1 string
-	{
-		indexResponses := createIndices(t, func() (*esapi.Response, error) {
-			id1 = strconv.Itoa(time.Now().Nanosecond())
-			return cli.Index(
-				titleIndex1,
-				bytes.NewBuffer(ReadFile(t, "data/file1.json")),
-				cli.Index.WithDocumentID(id1),
-				cli.Index.WithRefresh("true"),
-			)
-		})
-		fmt.Printf("Index creation responses:\n%s", StructToJSON(t, indexResponses))
-	}
-	{
-		query := fmt.Sprintf(`
-			{
-				"query": {
-					"match": {
-						"title": "%s"
-					}
-				 }
-			}
-		`, "Title1")
-		res, err := cli.Search(
-			cli.Search.WithContext(context.Background()),
-			cli.Search.WithIndex(titleIndex1),
-			cli.Search.WithBody(bytes.NewBuffer([]byte(query))),
-			cli.Search.WithTrackTotalHits(true),
-			cli.Search.WithPretty(),
-		)
-		require.NoError(t, err)
-		require.False(t, res.IsError())
-		js := BodyToJSON(t, res.Body)
-		fmt.Printf("SEARCH response:\n%s\n", js)
-		value := gjson.Get(js, "hits.total.value").Float()
-		took := gjson.Get(js, "took").Float()
-		fmt.Printf("[%s] %f hits; took: %fms", res.Status(), value, took)
-		hits := gjson.Get(js, "hits.hits").Array()
-		require.Greater(t, len(hits), 0)
-		for _, hit := range hits {
-			id := hit.Get("_id").Value()
-			src := hit.Get("_source").Value()
-			fmt.Printf(" * ID=%v, %v\n", id, src)
-		}
-	}
-	{
-		res, err := cli.Get(titleIndex1, id1)
-		require.NoError(t, err)
-		require.False(t, res.IsError())
-		js := BodyToJSON(t, res.Body)
-		fmt.Printf("GET response:\n%s\n", js)
-		require.True(t, gjson.Get(js, "found").Bool())
-	}
-}
-
-func getClient(t *testing.T) *es.Client {
-	t.Helper()
-	env := docker.StartEnvironment(
-		&docker.EnvironmentConfig{
-			UpTimeout:        60 * time.Second,
-			DownTimeout:      60 * time.Second,
-			ComposeFilePaths: []string{"docker-compose.yml"},
-		},
-		&docker.ServiceEntry{
-			Name:                "es01",
-			Handler:             GetElasticSearchClient,
-			DisableShutdownLogs: true,
-		},
-		&docker.ServiceEntry{
-			Name:                "es02",
-			DisableShutdownLogs: true,
-		},
-		&docker.ServiceEntry{
-			Name:                "kibana",
-			DisableShutdownLogs: true,
-		},
-	)
-	t.Cleanup(env.Shutdown)
-	return env.Services["es01"].(*es.Client)
 }
